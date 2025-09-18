a5c5d508936953787bdfecc9eff93569cf19a94e
inebw <punit965@gmail.com>
2025-09-18 20:16:27 +0530

    UI element added to game

--- script.js
+++ script.js
@@ -11,59 +11,77 @@ function getComputerChoice(){
 }
 
 // return the user choice
-function getHumanChoice(){
-    return prompt("Please enter your choice!\n- rock\n- paper\n- scissors")
+function getHumanChoice(e){
+    return e.target.alt
 }
 
-// to play the entire game of 5 rounds
-function playGame(){
+let humanScore = 0
+let computerScore = 0
 
-    // store the human and computer scores
-    let humanScore = 0
-    let computerScore = 0
 
-    // play a single round
-    function playRound(humanChoice, computerChoice){
-        if (humanChoice == computerChoice) {
-            alert("It's a tie!")
-        } else if (
-            (humanChoice == 'rock' && computerChoice == 'paper') || 
-            (humanChoice == 'paper' && computerChoice == 'scissors') ||
-            (humanChoice == 'scissors' && computerChoice == 'rock')){
-                alert(`You lose! ${computerChoice} beats ${humanChoice}`);
-                computerScore += 1;
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const gameStatusDiv = document.querySelector('.game-status');
+const gameStatus = document.createElement('p');
+const btn = document.querySelector(".game-buttons");
+const resetButton = document.createElement('p')
 
-        } else {
-            alert(`You won! ${humanChoice} beats ${computerChoice}`);
-            humanScore += 1;
-        }
-    }
-    alert("Let's start the game of Rock, Papers and Scissors\nThe game is of 5 rounds\nLet's Begin")
+btn.addEventListener('click', function playRound(e){
+    gameStatusDiv.appendChild(gameStatus);
+    gameStatus.style.display = 'flex';
+    let humanChoice = getHumanChoice(e);
+    let computerChoice = getComputerChoice();
+    const yourScore = document.querySelector('.your-score');
+    const compScore = document.querySelector('.computer-score');
+    if (humanChoice == undefined) {
+        gameStatus.textContent = "Please retry and click in the middle"
+        
+    } else if (humanChoice == computerChoice) {
+        gameStatus.textContent = "It's a tie!";
+    } else if (
+        (humanChoice == 'rock' && computerChoice == 'paper') || 
+        (humanChoice == 'paper' && computerChoice == 'scissors') ||
+        (humanChoice == 'scissors' && computerChoice == 'rock')){
+            gameStatus.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+            computerScore += 1;
+            
 
-    //loop for 5 times
-    for (let i = 0; i < 5; i++) {
-        alert(`Round ${i+1}: Make your choice`)
-        humanChoice = getHumanChoice().toLowerCase();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    }
-
-    if (humanScore > computerScore){
-        console.log(
-            `Congratulations! You won the game:
-            Your score: ${humanScore}
-            Computer Score: ${computerScore}`)
-    } else if(computerScore > humanScore) {
-        console.log(
-            `You Lost! Better Luck Next Time:
-            Your score: ${humanScore}
-            Computer Score: ${computerScore}`)
     } else {
-        console.log(`
-            Dang! It's a Tie
-            Your score: ${humanScore}
-            Computer Score: ${computerScore}`)
+        gameStatus.textContent = `You won! ${humanChoice} beats ${computerChoice}`;
+        humanScore += 1;
+        
+    }
+    compScore.textContent = "Computer Score: " + " " + computerScore;
+    yourScore.textContent = "Your Score: " + " " + humanScore;
+    if (computerScore == 5 || humanScore == 5) {
+        resetButton.style.display = 'flex';
+        gameStatus.textContent = computerScore == 5 ? "The computer has beaten You!" : "You Won!"
+        resetButton.textContent = 'Replay'
+        gameStatusDiv.appendChild(resetButton)
+        resetButton.addEventListener('click', () => {
+            computerScore = 0
+            humanScore = 0
+            gameStatus.style.display = 'none';
+            compScore.textContent = "Computer Score: " + " " + computerScore;
+            yourScore.textContent = "Your Score: " + " " + humanScore;
+            resetButton.style.display = 'none';
+        })
     }
 
+})
+const img = document.querySelectorAll('img');
 
-}
+img.forEach((elem) => {
+    elem.addEventListener('mouseover', (e) => {
+        const foc = e.target;
+        foc.classList.toggle('on-hover');
+})
+})
+
+img.forEach((elem) => {
+    elem.addEventListener('mouseout', (e) => {
+        const foc = e.target;
+        foc.classList.toggle('on-hover');
+})
+})
